8302b84ca3d5e9e839356763b424a128dc615619
Emma <emmakepple@gmail.com>
2018-09-29 14:25:19 -0500

    setting up modules and components

---
+++ webdev-project/src/app/announcements/announcements.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnnouncementsRoutingModule } from './announcements-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AnnouncementsRoutingModule
+  ],
+  declarations: []
+})
+export class AnnouncementsModule { }
